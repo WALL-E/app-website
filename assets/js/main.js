f591dd994e90902cd86435c8779ef1297b71e4e2
zhangzheng <zzgigi2003@163.com>
2018-04-09 13:51:07 +0800

    init project

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {         
+	
+	/* ======= Fixed Header animation ======= */ 
+	       
+    $(window).on('scroll', function() {
+         
+         if ($(window).scrollTop() > 0 ) {
+             $('#header').addClass('header-change');
+         }
+         else {
+             $('#header').removeClass('header-change');             
+         }
+    }); 
+    
+    /* ======= Toggle between Signup & Login Modals ======= */ 
+    $('#signup-link').on('click', function(e) {
+        $('#signup-modal').modal();
+        $('#login-modal').modal('toggle');
+        e.preventDefault();
+    });
+    
+    
+    $('#login-link').on('click', function(e) {
+        $('#login-modal').modal();
+        $('#signup-modal').modal('toggle');
+        e.preventDefault();
+    });
+    
+    
+    $('#myNavmenu').offcanvas({toggle:false, canvas:"body", autohide: false});
+    
+
+});
---
+++ server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -x
+set -e 
+
+python -m SimpleHTTPServer 8080
